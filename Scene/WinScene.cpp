b8e59dadf54eb58186fc4ac3fb13c234302976f3
Beth Tsai <bingyan0617@gmail.com>
2024-05-19 16:13:59 +0800

    Update Winscene
    show inputText

--- Scene/WinScene.cpp
+++ Scene/WinScene.cpp
@@ -1,6 +1,8 @@
 #include <functional>
+#include <iostream>
 #include <string>
 #include <sstream>
+#include <fstream>
 
 #include "Engine/AudioHelper.hpp"
 #include "Engine/GameEngine.hpp"
@@ -8,10 +10,10 @@
 #include "UI/Component/ImageButton.hpp"
 #include "UI/Component/Label.hpp"
 #include "PlayScene.hpp"
+#include "ScoreBoardScene.hpp"
 #include "Engine/Point.hpp"
 #include "Engine/Group.hpp"
 #include "WinScene.hpp"
-#include "PlayScene.hpp"
 #include "Engine/LOG.hpp"
 using Engine::LOG;
 using Engine::INFO;
@@ -47,6 +49,20 @@ void WinScene::Update(float deltaTime) {
 	}
 }
 void WinScene::BackOnClick(int stage) {
+	// Store inputText to the file
+	std::fstream f1, f2;
+
+	PlayScene* scenen = dynamic_cast<PlayScene*>(Engine::GameEngine::GetInstance().GetScene("play"));
+
+	f1.open("Resource/scoreboard.txt", std::ios::app);
+	if(!f1) LOG(INFO) << "File 1 not opened";
+	f1 << "\n" << inputText << " " << scenen->GetLives() * 10;
+	f1.close();
+
+	f2.open("..//Resource/scoreboard.txt", std::ios::app);
+	if(!f2) LOG(INFO) << "File 2 not opened";
+	f2 << "\n" << inputText << " " << scenen->GetLives() * 10;
+	f2.close();
 	// Change to select scene.
 	Engine::GameEngine::GetInstance().ChangeScene("start");
 }
@@ -55,17 +71,24 @@ void WinScene::OnKeyDown(int keyCode) {
 	IScene::OnKeyDown(keyCode);
 	//show the input after press any character
 	if(keyCode == ALLEGRO_KEY_BACKSPACE){
-		keyStrokes.pop_back();
+		LOG(INFO) << "Key Down: " << keyCode;
+		if(keyStrokes.size() > 0) keyStrokes.pop_back();
+		inputText = "";
+		for(auto c: keyStrokes){
+			inputText = inputText + (char)(c - ALLEGRO_KEY_A + 'A');
+		}
+		LOG(INFO) << "Input text: " << inputText;
 	} else if(keyCode >= ALLEGRO_KEY_A && keyCode <= ALLEGRO_KEY_Z){
 		LOG(INFO) << "Key Down: " << keyCode;
 		keyStrokes.push_back(keyCode);
-		ss << (char)(keyCode - ALLEGRO_KEY_A + 'A');
-		inputText = ss.str();
+		inputText = "";
+		for(auto c: keyStrokes){
+			inputText = inputText + (char)(c - ALLEGRO_KEY_A + 'A');
+		}
 		LOG(INFO) << "Input text: " << inputText;
 		keyCode = 0;
 
-	}
-	
+	}	
 }
 
 void WinScene::Draw() const {
@@ -75,8 +98,7 @@ void WinScene::Draw() const {
 	int h = Engine::GameEngine::GetInstance().GetScreenSize().y;
 	int halfW = w / 2;
 	int halfH = h / 2;
-	//LOG(INFO) << "Draw input text: " << inputText;
-	Engine::Label label("Hi", "pirulen.ttf", 48, halfW, halfH/4 + 30, 225, 225, 225, 1, 0.5, 0.5);
+	Engine::Label label(inputText, "pirulen.ttf", 48, halfW, halfH/4 + 50, 255, 255, 255, 255, 0.5, 0.5);
 	label.Anchor = Engine::Point(0.5, 0.5);	
 	label.Draw();
 }
